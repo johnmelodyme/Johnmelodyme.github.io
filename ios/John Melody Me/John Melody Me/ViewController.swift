67803e1b0c399f7058b2430586bb58c5b9ff44e1
John Melody Me <Johnmelodyme@yandex.com>
2020-05-26 01:04:41 +0800

    __init__

---
+++ ios/John Melody Me/John Melody Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  John Melody Me
+//
+//  Created by John Melody on 26/05/2020.
+//  Copyright © 2020 John Melody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
